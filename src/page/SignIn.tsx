d6f564e6e771feb4f8fdd0909e296933d7957188
mahmudulhasan5050 <50146955+mahmudulhasan5050@users.noreply.github.com>
2024-10-27 17:47:39 +0200

    auth google fix

--- src/page/SignIn.tsx
+++ src/page/SignIn.tsx
@@ -50,7 +50,7 @@ const SignIn = () => {
     }
 
     const handleGoogleSignin = () => {
-        window.location.href = 'http://localhost:5000/api/v1/auth/google'
+        window.location.href = `${process.env.REACT_APP_API}/auth/google`
     }
 
     return (
@@ -61,11 +61,6 @@ const SignIn = () => {
                     onClick={handleGoogleSignin}
                     className="flex items-center justify-center w-full px-4 py-2 text-sm font-medium text-blue-600 bg-white border border-blue-500 rounded-lg hover:bg-blue-100 focus:outline-none focus:ring"
                 >
-                    {/* <img
-                        src="https://www.svgrepo.com/show/355037/google.svg"
-                        alt="Google Icon"
-                        className="w-5 h-5 mr-2"
-                    /> */}
                     <FcGoogle className="w-5 h-5 mr-2"/>
                     Sign in with Google
                 </button>
